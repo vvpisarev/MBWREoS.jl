ffda211bb57e220cf41bb143217ec321ee484388
Степан Захаров <stepanzh@gmail.com>
2022-04-08 16:01:19 +0300

    Gas constant is imported

--- src/constants.jl
+++ src/constants.jl
@@ -1,7 +1,5 @@
-const GAS_CONSTANT_SI = 8.31446261815324 # Exact since May20, 2019; https://en.wikipedia.org/wiki/Gas_constant
-const PA_IN_BAR = 1e5               # Сколько паскалей в баре
-const BOLTZMANN_SI = 1.380649e-23 # Exact since May 20, 2019; https://en.wikipedia.org/wiki/Boltzmann_constant
-const C3_TC = 369.85 # propane critical temperature (K)
-const C3_VC = 2e-4 # propane critical volume (m³ / mol)
-const C3_ZC = 0.27626 # propane critical Z-factor (dimensionless)
-const C3_OMEGA = 0.1524 # propane acentric factor
+const GAS_CONSTANT_SI = CubicEoS.GAS_CONSTANT_SI
+const C3_TC = 369.85        # propane critical temperature (K)
+const C3_VC = 2e-4          # propane critical volume (m³ / mol)
+const C3_ZC = 0.27626       # propane critical Z-factor (dimensionless)
+const C3_OMEGA = 0.1524     # propane acentric factor
