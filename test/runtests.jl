c766019cb92ae90b015bde9d57cc2229de8e31fd
Степан Захаров <stepanzh@gmail.com>
2022-04-11 12:07:06 +0300

    Test for vt_stability

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,10 @@ volume = 1e-6
 nmol = 5000 * [0.4, 0.6] * volume
 RT = CubicEoS.GAS_CONSTANT_SI * 300
 
-MBWREoS.log_c_activity(c1c5, nmol, volume, RT)
+logca = CubicEoS.log_c_activity(c1c5, nmol, volume, RT)
+logca = CubicEoS.log_c_activity!(logca, c1c5, nmol, volume, RT)
+
+logca, jacobian = CubicEoS.log_c_activity_wj(c1c5, nmol, volume, RT)
+logca, jacobian = CubicEoS.log_c_activity_wj!(logca, jacobian, c1c5, nmol, volume, RT)
+
+vt_stability(c1c5, nmol, volume, RT, CubicEoS.VTStabilityIdealIdentityState)
